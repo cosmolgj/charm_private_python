85c87f6922d4dcb08c41ac10d990b8a741958a13
cosmolgj <cosmolgj@empal.com>
2021-10-21 12:54:51 +0900

    211021_office

---
+++ Class/decorator.py
@@ -0,0 +1,26 @@
+def decorator1(func):
+    def wrapper():
+        print("decorator1")
+        func()
+
+    return wrapper
+
+
+def decorator2(func):
+    def wrapper():
+        print("decorator2")
+        func()
+
+    return wrapper
+
+
+@decorator1
+@decorator2
+def hello():
+    print("hello")
+
+
+#hello()
+
+decorated_hello = decorator1(decorator2(hello))
+decorated_hello()
